b5bf69d25867f8f5b5f78ac4cf31d6e52d8d10d7
Sosed23 <sosed2393@gmail.com>
2025-09-10 10:52:33 +0300

    Add extra="allow"

--- bot/config.py
+++ bot/config.py
@@ -31,7 +31,8 @@ class Settings(BaseSettings):
     
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(
-            os.path.abspath(__file__)), "..", ".env")
+            os.path.abspath(__file__)), "..", ".env"),
+        extra="allow"
     )
 
 # Получаем параметры для загрузки переменных среды
