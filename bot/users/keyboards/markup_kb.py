f4ce4c9fbdc4d6f4e46081c33f70fdf2a071f640
Sosed23 <sosed2393@gmail.com>
2025-09-10 10:35:51 +0300

    Add webapp_url

--- bot/users/keyboards/markup_kb.py
+++ bot/users/keyboards/markup_kb.py
@@ -1,10 +1,11 @@
 from aiogram.types import ReplyKeyboardMarkup, WebAppInfo
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from bot.config import webapp_url
 
 
 def back_keyboard() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
-    kb.button(text="Фильтр моделей", web_app=WebAppInfo(url="http://212.109.221.68:1111/webapp"))
+    kb.button(text="Фильтр моделей", web_app=WebAppInfo(url=webapp_url))
     kb.button(text="✨ Поиск с ИИ")
     kb.button(text="🔍 Поиск модели")
     kb.button(text="🛒 Корзина")
